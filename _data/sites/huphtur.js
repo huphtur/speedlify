68371fb51a71fc1512c00358c4596a9903f4a042
M. Appelman <huphtur@icloud.com>
2020-10-14 17:22:39 -0700

    personal and work sites 🏡 🏢

---
+++ _data/sites/huphtur.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "huphtur", // optional, falls back to object key
+	description: "huphtur",
+	options: {
+		frequency: 60, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://huphtur.nl/",
+		"https://skateorbike.com/"
+	]
+};
---
+++ _data/sites/networkninja.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Network Ninja", // optional, falls back to object key
+	description: "Network Ninja",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://maineventsoftware.com/",
+		"https://networkninja.com/",
+		"https://stoptxeviction.org/",
+		"https://www.collaboratesoftware.com/",
+		"https://www.legalserver.org/",
+	]
+};
