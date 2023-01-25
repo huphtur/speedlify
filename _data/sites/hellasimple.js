f4256c6923364c0dde9b4a5ed26cdde6ad2ee9d6
M. Appelman <huphtur@icloud.com>
2023-01-25 13:02:21 -0800

    add hella simple starter

---
+++ _data/sites/hellasimple.js
@@ -0,0 +1,15 @@
+module.exports = {
+  description: 'Hella Simple Starter',
+  options: {
+    frequency: 60 * 23 * 7, // almost weekly
+    freshChrome: 'site',
+  },
+  options: {
+    frequency: 1,
+    freshChrome: 'site',
+  },
+  urls: [
+    'https://hella-simple-eleventy-tailwindcss.netlify.app/',
+  ],
+  skip: false,
+};
