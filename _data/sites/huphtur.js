16415c6d7908024e8df35b1d739a922dd2c21bc8
M. Appelman <huphtur@icloud.com>
2023-01-24 17:02:31 -0800

    add huphtur.nl config remove samples

---
+++ _data/sites/huphtur.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'huphtur.nl',
+  description: 'Personal website',
+  options: {
+    frequency: 60 * 23, // 23 hours
+    freshChrome: 'site',
+  },
+  options: {
+    frequency: 1,
+    freshChrome: 'site',
+  },
+  urls: [
+    'https://huphtur.nl/',
+    'https://huphtur.nl/about/',
+    'https://huphtur.nl/archives/',
+  ],
+  skip: false,
+};
